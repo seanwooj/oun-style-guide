a822a0299d23b0d8f9cbe2dcf148152a39c8077f
seanwooj <seanwoojinkim@gmail.com>
2016-01-29 14:05:18 -0800

    scroll works within the sidenav" git push

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,22 @@
 'use strict';
 $(function () {
+  // set height of bridge menu such that content can scroll within it (prevent height:auto)
+  var windowHeight = $(window).height();
+  var topAreaHeight = $('.bridge-top-area').height();
+  var offSetHeight = windowHeight - topAreaHeight;
+  $('.bridge-navigation-area').height(offSetHeight);
+
+  
   // this is all just going to be loose javascript, as it will never be production code
   $('.toggle-bridge').on('click', function(){
     $('.bridge-menu').toggle('slide');
+    
+    // set height of bridge menu such that content can scroll within it (prevent height:auto)
+    var windowHeight = $(window).height();
+    var topAreaHeight = $('.bridge-top-area').height();
+    var offSetHeight = windowHeight - topAreaHeight;
+    $('.bridge-navigation-area').height(offSetHeight);
+
   });
 
   $('.content-item-expansion-area').on('click', function (e) {
