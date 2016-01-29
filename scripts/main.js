a66f5020d24ade11ec4bbcfb6910790f85075ab9
seanwooj <seanwoojinkim@gmail.com>
2016-01-29 14:17:53 -0800

    added fake markup

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,5 @@
 'use strict';
 $(function () {
-  // set height of bridge menu such that content can scroll within it (prevent height:auto)
-  var windowHeight = $(window).height();
-  var topAreaHeight = $('.bridge-top-area').height();
-  var offSetHeight = windowHeight - topAreaHeight;
-  $('.bridge-navigation-area').height(offSetHeight);
-
-  
   // this is all just going to be loose javascript, as it will never be production code
   $('.toggle-bridge').on('click', function(){
     $('.bridge-menu').toggle('slide');
@@ -19,7 +12,14 @@ $(function () {
 
   });
 
-  $('.content-item-expansion-area').on('click', function (e) {
+  $(document).on('click', '.content-item-expansion-area', function (e) {
     $(e.target).closest('.content-item').toggleClass('closed');
   });
+
+  // i dont' want to write duplicate markup, so i'm just going to clone elements here
+
+  for(var i = 0; i < 8; i += 1) {
+    var $lesson = $('.top-level-item').first().clone();
+    $('.bridge-lessons').append($lesson);
+  }
 });
